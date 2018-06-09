fc86be9b9e952f4ca37ff6a9e9167432b9bf848b
Andrew <aolivier@ur.rochester.edu>
2018-06-09 13:57:54 +0200

    Added default configuration files for all Reconstructors and Analyzers with the comments from old command line configuration system. Added an example YAML job configuration and confirmed that it runs with "new edep-sim".

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 export LD_LIBRARY_PATH="`pwd`/`dirname $BASH_SOURCE`/../lib:${LD_LIBRARY_PATH}"
 #Tell 3DSTNeutrons where default configuration files are installed
 export THREEDSTNEUTRONS_CONF_PATH="`pwd`/`dirname $BASH_SOURCE`/../conf/"
-export PATH=$PATH:"`pwd`/`dirname $BASH_SOURCE`/../bin"
+#Make sure bash finds NeutronApp by defaultexport PATH=$PATH:"`pwd`/`dirname $BASH_SOURCE`/../bin"
